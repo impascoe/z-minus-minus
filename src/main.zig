43af7b1f4e9dc234b856f38877dd632abe25f7e6
Isaac Pascoe <IsaacMPascoe@gmail.com>
2025-01-28 11:59:23 +0800

    updated .gitignore

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,16 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    defer std.process.argsFree(std.heap.page_allocator, args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const argc = args.len;
+    const argv = args;
 
-    try bw.flush(); // don't forget to flush!
+    std.debug.print("These are the number of arguments {s}\n", .{argc});
+    std.debug.print("These are the arguments {s}\n", .{argv});
+
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+
